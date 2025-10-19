20b36f59f3d26f36bb1e2d13f11b7a6fb6b45b30
mubin6th <mubin.muhammad.the.7th@gmail.com>
2025-10-19 20:33:33 +0600

    initial commit.

---
+++ README.md
@@ -0,0 +1,26 @@
+<div align="center">
+    <h1>charcoal.nvim</h1>
+    <p>A simple monochrome colorscheme.</p>
+    <p><i>
+        This repository is derived from
+        <a href="https://github.com/mubin6th/charcoal">mubin6th/charcoal</a>.
+    <i></p>
+
+**[Preview] | [Install] | [Contribute]**
+</div>
+
+[Preview]: https://github.com/mubin6th/charcoal.nvim?tab=readme-ov-file#preview
+[Install]: https://github.com/mubin6th/charcoal.nvim?tab=readme-ov-file#install
+[Contribute]: https://github.com/mubin6th/charcoal.nvim?tab=readme-ov-file#contribute
+
+## About
+_todo_
+
+## Preview
+_todo_
+
+## Install
+_todo_
+
+## Contribute
+_todo_
---
+++ colors/charcoal.lua
@@ -0,0 +1,56 @@
+-- =============================================================================
+-- Name:           Charcoal
+-- Description:    A simple monochrome colorscheme.
+-- Author:         mubin6th
+-- Website:        https://github.com/mubin6th/charcoal.nvim
+-- License:        MIT
+-- =============================================================================
+
+local palette
+
+if vim.o.background == 'dark' then
+    palette = {
+        base00 = "#0f0b05",
+        base01 = "#231b0e",
+        base02 = "#2a2012",
+        base03 = "#57462c",
+        base04 = "#a88c62",
+        base05 = "#c3a983",
+        base06 = "#dec8a7",
+        base07 = "#231b0e",
+        base08 = "#a88c62",
+        base09 = "#dec8a7",
+        base0A = "#dec8a7",
+        base0B = "#dec8a7",
+        base0C = "#dec8a7",
+        base0D = "#c3a983",
+        base0E = "#a88c62",
+        base0F = "#876e48",
+    }
+end
+
+if vim.o.background == 'light' then
+    palette = {
+        base00 = "#cabda0",
+        base01 = "#bcad8c",
+        base02 = "#af9f7d",
+        base03 = "#645538",
+        base04 = "#110e06",
+        base05 = "#382e1b",
+        base06 = "#4b3e26",
+        base07 = "#bcad8c",
+        base08 = "#382e1b",
+        base09 = "#110e06",
+        base0A = "#110e06",
+        base0B = "#110e06",
+        base0C = "#110e06",
+        base0D = "#251e0f",
+        base0E = "#382e1b",
+        base0F = "#4b3e26",
+    }
+end
+
+if palette then
+    require("mini.base16").setup({ palette = palette, use_cterm = true })
+    vim.g.colors_name = "charcoal"
+end
